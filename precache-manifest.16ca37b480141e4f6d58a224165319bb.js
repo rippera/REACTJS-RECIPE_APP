8646f32437a0217b25551a435cb47b323a312686
rippera <mikheil.mezurnishvili10b@geolab.edu.ge>
2019-06-29 16:33:43 +0400

    Updates

---
+++ precache-manifest.16ca37b480141e4f6d58a224165319bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0b6cd10f370c04b67ccae5565f5ddd",
+    "url": "/REACTJS-RECIPE_APP/index.html"
+  },
+  {
+    "revision": "4b2c91b8e45284eb66b7",
+    "url": "/REACTJS-RECIPE_APP/static/css/main.3abacae3.chunk.css"
+  },
+  {
+    "revision": "f030f535425b9a9eeb9e",
+    "url": "/REACTJS-RECIPE_APP/static/js/2.71869fdd.chunk.js"
+  },
+  {
+    "revision": "4b2c91b8e45284eb66b7",
+    "url": "/REACTJS-RECIPE_APP/static/js/main.06e1fc67.chunk.js"
+  },
+  {
+    "revision": "539cdf19d180c5c25d90",
+    "url": "/REACTJS-RECIPE_APP/static/js/runtime~main.8dd82f76.js"
+  }
+]);
